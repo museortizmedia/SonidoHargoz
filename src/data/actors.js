b9a5c40ea6550835817646d6807b800d991ca38e
Diego Ortiz <73130246+museortizmedia@users.noreply.github.com>
2026-02-25 11:52:34 -0500

    Archvios

--- src/data/actors.js
+++ src/data/actors.js
@@ -1,12 +1,50 @@
-import actor1 from "../assets/actor1.jpg";
-import demo1 from "../assets/demo1.mp3";
+import actor1 from "../assets/actores/actor1.jpg";
+import actor2 from "../assets/actores/actor2.jpg";
+import actor3 from "../assets/actores/actor3.jpg";
+import actor4 from "../assets/actores/actor4.jpg";
+import actor5 from "../assets/actores/actor5.jpg";
+
+import demo1 from "../assets/demos/demo1.mp3";
+import demo2 from "../assets/demos/demo2.mp3";
+import demo3 from "../assets/demos/demo3.mp3";
+import demo4 from "../assets/demos/demo4.mp3";
+import demo5 from "../assets/demos/demo5.mp3";
 
 export const actors = [
     {
         id: 1,
-        name: "María Torres",
+        name: "Luisana 'Luka' Otero",
         description: "Especialista en narración profunda y dramática.",
         image: actor1,
         audio: demo1,
+    },
+    {
+        id: 2,
+        name: "Santiago Zambrano",
+        description: "Soy Santiago Zambrano Rivera, un joven de 23 años de edad, Comunicador Social y Periodista con especialización en Comunicación y Periodismo Digital, egresado de la Universidad Autónoma de Occidente y actualmente estudiante del programa de Educación para el Trabajo y Desarrollo Humano (ETDH) en Música en la Institución Universitaria IUIPC de la ciudad de Cali./nCuento con experiencia en componentes audiovisuales y radiofónicos en producción y creación de contenidos de video y sonido para radio, televisión y ecosistemas digitales./nHe sido ganador del primer lugar en los premios 'Radio del Nuevo Siglo 2023' de la Universidad de La República, en Montevideo Uruguay en la categoría 'Pódcast de ficción' con el programa sonoro interactivo 'La Profesía', un trabajo realizado en conjunto con los estudiantes de la materia Podcasting Interactivo de la UAO y ganador de premios musicales universitarios como: 1er lugar - Categoría Solista Masculino en el XXVIII Festival Interno de La Canción 2024, 1er lugar en la categoría Solista Masculino en el Festival Regional de La Canción - ASCUN Occidente 2024 y 1er lugar en la categoría Grupo Fusión con Sonata Jazz de la UAO en el Festival Regional de Orquestas, Bandas y Grupos Fusión - ASCUN Occidente 2024./nSoy un gran apasionado a la radio, y mi paso por Ondauao en la realización radiofónica en E-Spodcast, ha hecho en mi, un profesional con un amplio recorrido en locución y manejo del Español Neutro Colombiano.",
+        desc: "Especialista en narración profunda y dramática.",
+        image: actor2,
+        audio: demo2,
+    },
+    {
+        id: 3,
+        name: "Antonio 'Tony' Arzuza",
+        description: "Especialista en narración profunda y dramática.",
+        image: actor3,
+        audio: demo3,
+    },
+    {
+        id: 4,
+        name: "Valentina Toro",
+        description: "Especialista en narración profunda y dramática.",
+        image: actor4,
+        audio: demo4,
+    },
+    {
+        id: 5,
+        name: "Nicolás Barrera",
+        description: "Especialista en narración profunda y dramática.",
+        image: actor5,
+        audio: demo5,
     },
 ];
